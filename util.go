0cddef88514994539389b68775e64b1673812618
ashinchen <ashinchen@tencent.com>
2019-09-13 22:51:43 +0800

    add GetOutboundIP util func

--- util.go
+++ util.go
@@ -1,6 +1,7 @@
 package netpro
 
 import (
+	"net"
 	"reflect"
 	"runtime"
 )
@@ -8,4 +9,17 @@ import (
 // GetFunctionName return the name of the function
 func GetFunctionName(i interface{}) string {
 	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+}
+
+// GetOutboundIP preferred outbound ip of this machine
+func GetOutboundIP() (net.IP, error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP, nil
 }
