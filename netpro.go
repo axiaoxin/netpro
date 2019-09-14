1c124ab0e49730cc6b1abb2b6cfed12ffb549d21
ashinchen <ashinchen@tencent.com>
2019-09-14 13:59:44 +0800

    add comment

--- netpro.go
+++ netpro.go
@@ -1,3 +1,9 @@
+// Package netpro means the abbreviation of golang net programming.
+// netpro can be used to build a network server conveniently,
+// just create the corresponding server, then specify the running port and implement the corresponding HandlerFunc processing function.
+// default load configuration file in current directory which named "config", you can also start the program through the command line parameters "config_path" and "config_name" to specify the path and name of the configuration file
+// the name of the configuration file does not contain suffix name, support JSON, TOML, YAML, HCL, envelope file or Java properties format.
+// refer to specific configuration items through "config.toml.example"
 package netpro
 
 import (
