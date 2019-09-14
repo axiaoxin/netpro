e057fd27eeab3a048283bf6a284ab154fbd05f71
ashinchen <ashinchen@tencent.com>
2019-09-14 13:31:41 +0800

    support use cmd set config path and name

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.12
 require (
 	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.4.2
+	github.com/spf13/pflag v1.0.3
 	github.com/spf13/viper v1.4.0
 )
--- viper.go
+++ viper.go
@@ -4,20 +4,31 @@ import (
 	"os"
 
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
 func initViper() {
 	// 确保viper最先被init
-	viper.SetConfigName("config")
+
+	// 设置默认值
 	viper.SetDefault("runtime.goroutine_num", 30000)
-	cwd, err := os.Getwd()
+
+	// 读取命令行参数
+	workdir, err := os.Getwd()
 	if err != nil {
-		logrus.Error("get work dir error:", err)
+		logrus.Fatal(err)
 	}
-	viper.AddConfigPath(cwd)
+	configPath := pflag.String("config_path", workdir, "Specify the path of the configuration file")
+	configName := pflag.String("config_name", "config", "Configuration file names that do not contain suffix names. (Support JSON, TOML, YAML, HCL, envfile or Java properties format)")
+	pflag.Parse()
+
+	// 读取配置
+	viper.SetConfigName(*configName)
+	viper.AddConfigPath(*configPath)
 	err = viper.ReadInConfig()
 	if err != nil {
-		logrus.Error("viper read in config error:", err)
+		logrus.Fatal("viper read in config error:", err)
 	}
+	logrus.Debugf("loaded %s in %s\n", *configName, *configPath)
 }
